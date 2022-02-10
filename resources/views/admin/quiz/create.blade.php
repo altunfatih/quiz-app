262a2f0478c956bead16aa5c57b30205bb60b2e6
Fatih <fatih.altun00@hotmail.com>
2022-02-10 12:15:09 +0300

    edited error and success message

--- resources/views/admin/quiz/create.blade.php
+++ resources/views/admin/quiz/create.blade.php
@@ -6,19 +6,19 @@
                 @csrf
                 <div class="form-group">
                     <label>Quiz Başlığı</label>
-                    <input type="text" name="title" class="form-control" required>
+                    <input type="text" name="title" class="form-control" value="{{ old('title') }}">
                 </div>
                 <div class="form-group">
                     <label>Quiz Açıklama</label>
-                    <textarea name="description" class="form-control" rows="4"></textarea>
+                    <textarea name="description" class="form-control" rows="4">{{ old('description') }}</textarea>
                 </div>
                 <div class="form-group">
                     <label>Bitiş Tarihi Olacak mı?</label>
-                    <input id="isFinished" type="checkbox">
+                    <input id="isFinished" @if(old('finished_at')) checked @endif type="checkbox">
                 </div>
-                <div id='finishedInput' style="display: none" class="form-group">
+                <div id='finishedInput' @if(!old('finished_at')) style="display: none" @endif class="form-group">
                     <label>Bitiş Tarihi</label>
-                    <input type="datetime-local" name="finished_at" class="form-control">
+                    <input type="datetime-local" name="finished_at" class="form-control" value="{{ old('finished_at') }}">
                 </div>
                 <div class="form-group">
                     <button type="submit" class="btn btn-success btn-sm btn-block">Quiz Oluştur</button>
