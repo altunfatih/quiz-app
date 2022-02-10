f88f18e776e2ad9c7807c829859debc948600ff1
Fatih <fatih.altun00@hotmail.com>
2022-02-10 17:55:01 +0300

    questions route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\QuestionController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\QuizController;
 
@@ -25,4 +26,5 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 Route::group(['middleware' => ['auth', 'isAdmin'], 'prefix' => 'admin'], function () {
     Route::get('quizzes/{id}', [QuizController::class, 'destroy'])->whereNumber('id')->name('quizzes.destroy');
     Route::resource('quizzes', QuizController::class);
+    Route::resource('quiz/{quiz_id}/questions', QuestionController::class);
 });
