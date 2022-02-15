4edf788b3d178626e7e3baf6378e5f66a7bd14b0
Fatih <fatih.altun00@hotmail.com>
2022-02-15 21:48:16 +0300

    sluggable added

--- app/Models/Quiz.php
+++ app/Models/Quiz.php
@@ -4,13 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Quiz extends Model
 {
     use HasFactory;
+    use Sluggable;
+
     protected $fillable = ['title', 'description', 'finished_at'];
 
     public function questions() {
         return $this->hasMany('App\Models\Questions');
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
