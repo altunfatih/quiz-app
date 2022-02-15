38f0743c99eeee7e2b4e2bc01a08534356266417
Fatih <fatih.altun00@hotmail.com>
2022-02-15 21:48:31 +0300

    edited dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,23 @@
 <x-app-layout>
     <x-slot name="header">Anasayfa</x-slot>
-
-    <div class="alert alert-danger">Bootstrap alert message</div>
+    <div class="rof">
+        <div class="col-md-8">
+            <div class="list-group">
+                @foreach ($quizzes as $quiz)
+                <a href="{{ route('quiz.detail', $quiz->slug) }}" class="list-group-item list-group-item-action flex-column align-items-start">
+                  <div class="d-flex w-100 justify-content-between">
+                    <h5 class="mb-1">{{ $quiz->title }}</h5>
+                  </div>
+                  <p class="mb-1">{{ Str::limit($quiz->description, 100) }}</p>
+                  <small>{{ $quiz->questions_count }}</small>
+                </a>
+                @endforeach
+                <div class="mt-2">
+                    {{ $quizzes->links() }}
+                </div>
+              </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+    </div>
 </x-app-layout>
